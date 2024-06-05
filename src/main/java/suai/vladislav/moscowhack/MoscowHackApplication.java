5821eec3bf6016a8e7cba5fed6fb74565d1eb3d3
ANANAzZzZz <vladscooltrol@gmail.com>
2024-06-05 14:18:04 +0300

    initial commit

---
+++ src/main/java/suai/vladislav/moscowhack/MoscowHackApplication.java
@@ -0,0 +1,13 @@
+package suai.vladislav.moscowhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoscowHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoscowHackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/suai/vladislav/moscowhack/MoscowHackApplicationTests.java
@@ -0,0 +1,13 @@
+package suai.vladislav.moscowhack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoscowHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
