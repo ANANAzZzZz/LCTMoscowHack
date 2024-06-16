f3e42fc034fc7b272bf39b72dafd10900b23288f
Andrewmlshv <andrewmlshv@mail.ru>
2024-06-16 14:59:49 +0300

    add swagger configuration

--- src/main/java/suai/vladislav/moscowhack/MoscowHackApplication.java
+++ src/main/java/suai/vladislav/moscowhack/MoscowHackApplication.java
@@ -2,8 +2,10 @@ package suai.vladislav.moscowhack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MoscowHackApplication {
 
     public static void main(String[] args) {
